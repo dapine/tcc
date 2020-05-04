f6c7a59a37fbc247e77c4a3927f20bbed4260f66
Breno Pinheiro <davepinh@gmail.com>
2020-05-04 20:41:56 -0300

    to-conll-2003.py adicionado. Contudo, se deve ter uma palavra por linha, o HAREM contém entidades de múltiplas palavras, então tratar isso

---
+++ to-conll-2003.py
@@ -0,0 +1,31 @@
+from xml.dom.minidom import parse
+
+xml = parse('harem.xml')
+
+docs = xml.getElementsByTagName('DOC')
+
+for doc in docs:
+    print('-DOCSTART- -X- O O\n')
+    cn = doc.childNodes
+    for n in cn:
+        if n.nodeType != n.TEXT_NODE:
+            p = n.childNodes
+            for em in p:
+                if em.nodeType != em.TEXT_NODE:
+                    cat = em.getAttribute('CATEG')
+                    valor = em.firstChild.nodeValue
+                    if cat == None or valor == None or cat.isspace() or valor.isspace() or cat == '' or valor == '':
+                        pass
+                    else:
+                        if '|' in cat:
+                            newcat = cat.split('|')[0]
+                            print('{0} -X- O {1}'.format(valor.strip(), newcat))
+                        else:
+                            print('{0} -X- O {1}'.format(valor.strip(), cat))
+                else:
+                    toks = em.nodeValue.split()
+                    for tok in toks:
+                        if tok != '':
+                            print('{0} -X- O {1}'.format(tok, 'O'))
+        else:
+            pass
