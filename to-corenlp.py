0f840c35d84a4b7ec532abfdd37b47d291ee8010
Breno Pinheiro <davepinh@gmail.com>
2020-05-10 14:47:41 -0300

    to-corenlp melhorado

--- to-corenlp.py
+++ to-corenlp.py
@@ -1,33 +1,33 @@
-from xml.dom.minidom import parse
+import bs4
+import nltk
+from bs4 import BeautifulSoup
 
-xml = parse('harem.xml')
+f = open('harem-utf8.xml', 'r')
 
-docs = xml.getElementsByTagName('DOC')
+soup = BeautifulSoup(f, 'lxml')
+
+docs = soup.find_all('doc')
+
+def pp(p):
+    if type(p) is bs4.element.Tag:
+        if p.name == 'em':
+            categ = p.get('categ')
+            if categ is None:
+                print(p.string, end='')
+            else:
+                if '|' in categ:
+                    categ = categ.split('|')[0]
+
+                print('{0}\t{1}'.format(p.string, categ))
+        else:
+            for x in p.contents:
+                pp(x)
+    else:
+        tokens = nltk.word_tokenize(p.string)
+        for tok in tokens:
+            print('{0}\t{1}'.format(tok, 'O'))
 
 for doc in docs:
-    cn = doc.childNodes
-    for n in cn:
-        if n.nodeType != n.TEXT_NODE:
-            p = n.childNodes
-            for em in p:
-                if em.nodeType != em.TEXT_NODE:
-                    cat = em.getAttribute('CATEG')
-                    valor = em.firstChild.nodeValue
-                    if cat == None or valor == None or cat.isspace() or valor.isspace() or cat == '' or valor == '':
-                        pass
-                    else:
-                        if '|' in cat:
-                            newcat = cat.split('|')[0]
-                            print('{0}\t{1}'.format(valor.strip(), newcat))
-                        else:
-                            print('{0}\t{1}'.format(valor.strip(), cat))
-                else:
-                    toks = em.nodeValue.split()
-                    for tok in toks:
-                        if tok != '':
-                            print('{0}\t{1}'.format(tok, 'O'))
-                    # print(em.nodeValue)
-        else:
-            pass
-            # print(n)
-            # print(n.nodeValue)
+    for p in doc.contents:
+        pp(p)
+        print()
