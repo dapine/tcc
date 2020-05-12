cbb2df5f5b15fcede0ceb056ef32a62ad3d6c505
Breno Pinheiro <davepinh@gmail.com>
2020-05-12 19:53:57 -0300

    Testes removendo pontuação

--- to-opennlp.py
+++ to-opennlp.py
@@ -11,24 +11,34 @@ soup = BeautifulSoup(f, 'lxml')
 
 docs = soup.find_all('doc')
 
-def pp(p):
+def remover_pontuacao(string):
+    tokens = nltk.word_tokenize(string)
+
+    words = [t for t in tokens if t.isalnum()]
+
+    return ' '.join(words)
+
+def pp(p, pontuacao=True):
     if type(p) is bs4.element.Tag:
         if p.name == 'em':
+            texto = p.string
             categ = p.get('categ')
             if categ is None:
-                print(p.string, end='')
+                print(texto, end='')
             else:
                 if '|' in categ:
                     categ = categ.split('|')[0]
 
-                print(' <START:{0}> {1} <END> '.format(categ, p.string), end='')
+                print(' <START:{0}> {1} <END> '.format(categ, texto), end='')
         else:
             for x in p.contents:
                 pp(x)
     else:
-        tokens = nltk.word_tokenize(p.string)
-        print(' '.join(tokens), end='')
-        # print(p.string, end='')
+        if pontuacao:
+            tokens = nltk.word_tokenize(p.string)
+            print(' '.join(tokens), end='')
+        else:
+            print(remover_pontuacao(p.string), end='')
 
 for doc in docs:
     for p in doc.contents:
