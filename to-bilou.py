5b59944f7f3393267e85b28848da8f2077819231
Breno Pinheiro <davepinh@gmail.com>
2020-05-19 21:09:21 -0300

    to-bilou.py adicionado e testes com spaCy

---
+++ to-bilou.py
@@ -0,0 +1,48 @@
+import bs4
+import nltk
+import sys
+from bs4 import BeautifulSoup
+
+filename = sys.argv[1]
+
+f = open(filename, 'r')
+
+soup = BeautifulSoup(f, 'lxml')
+
+docs = soup.find_all('doc')
+
+def pp(p):
+    if type(p) is bs4.element.Tag:
+        if p.name == 'em':
+            categ = p.get('categ')
+            valor = p.string
+            if categ is None:
+                print(valor, end='')
+            else:
+                if '|' in categ:
+                    categ = categ.split('|')[0]
+
+                if ' ' in valor:
+                    valor = valor.split()
+
+                    for i, v in enumerate(valor):
+                        if i == 0:
+                            print('{0} B-{1}'.format(v, categ))
+                        elif i == len(valor)-1:
+                            print('{0} L-{1}'.format(v, categ))
+                        else:
+                            print('{0} I-{1}'.format(v, categ))
+                else:
+                    print('{0} U-{1}'.format(valor, categ))
+        else:
+            for x in p.contents:
+                pp(x)
+    else:
+        tokens = nltk.word_tokenize(p.string)
+        for tok in tokens:
+            print('{0} {1}'.format(tok, 'O'))
+
+for doc in docs:
+    for p in doc.contents:
+        pp(p)
+        print()
