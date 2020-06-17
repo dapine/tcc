92da73c91638dd596668b25e9c9386162e80bc07
Breno Pinheiro <davepinh@gmail.com>
2020-06-16 21:33:22 -0300

    Melhor divisão treino/teste para corenlp

--- corenlp.py
+++ corenlp.py
@@ -1,7 +1,10 @@
 import bs4
 import sys
+import random
 from bs4 import BeautifulSoup
 from harem import *
+
+stdout = sys.stdout
 
 filename = 'harem.xml'
 # filename = sys.argv[1]
@@ -25,27 +28,39 @@ def todos_paragrafos(docs):
 
     return par
 
+def print_ps(paragrafos):
+    for p in paragrafos:
+        for t in p.lista:
+            if t.t == 'em':
+                cat = t.categoria
+                if t.categoria != None and '|' in t.categoria:
+                    cat = t.categoria.split('|')[0]
+
+                tokens = t.texto.split()
+
+                for tok in tokens:
+                    print('{}\t{}'.format(tok, cat))
+            else:
+                tokens = t.texto.split()
+
+                for tok in tokens:
+                    print('{}\t{}'.format(tok, 'O'))
+
+def print_file(paragrafos, arquivo):
+    with open(arquivo, 'w') as f:
+        sys.stdout = f
+        print_ps(paragrafos)
+        sys.stdout = stdout
+
+def treino_teste(paragrafos, n):
+    random.shuffle(paragrafos)
+
+    treino = paragrafos[0:n]
+    teste = paragrafos[n+1:len(paragrafos)]
+
+    print_file(treino, 'tmp/corenlp/harem-100.train')
+    print_file(teste, 'tmp/corenlp/harem-100.test')
+
 paragrafos = todos_paragrafos(documentos)
 
-# 2773
-p_min = 0
-p_max = 772
-
-limite_p = paragrafos[p_min:p_max]
-
-for p in limite_p:
-    for t in p.lista:
-        if t.t == 'em':
-            cat = t.categoria
-            if t.categoria != None and '|' in t.categoria:
-                cat = t.categoria.split('|')[0]
-
-            tokens = t.texto.split()
-
-            for tok in tokens:
-                print('{}\t{}'.format(tok, cat))
-        else:
-            tokens = t.texto.split()
-
-            for tok in tokens:
-                print('{}\t{}'.format(tok, 'O'))
+treino_teste(paragrafos, 2000)
