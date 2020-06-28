3720bb6c233ec621708af52d2e673b384a849e9e
Breno Pinheiro <davepinh@gmail.com>
2020-06-28 20:26:42 -0300

    spaCy melhorado e média de palavras e entidades por parágrafo

--- iob.py
+++ iob.py
@@ -6,9 +6,10 @@ from harem import *
 
 stdout = sys.stdout
 
-# filename = 'harem.xml'
-opcao = sys.argv[1]
-filename = sys.argv[2]
+filename = sys.argv[1]
+opcao = sys.argv[2]
+variante = sys.argv[3]
+porcentagem = sys.argv[4]
 
 f = open(filename, 'r')
 
@@ -55,41 +56,6 @@ def print_ps(paragrafos):
                     for tok in tokens:
                         print('{0} -X- O {1}'.format(tok, 'O'))
 
-def print_file(paragrafos, arquivo):
-    with open(arquivo, 'w') as f:
-        sys.stdout = f
-        print_ps(paragrafos)
-        sys.stdout = stdout
-
-def treino_teste(paragrafos, n):
-    random.shuffle(paragrafos)
-
-    treino = paragrafos[0:n]
-    teste = paragrafos[n+1:len(paragrafos)]
-
-    print_file(treino, 'tmp/spacy/harem-100.train')
-    print_file(teste, 'tmp/spacy/harem-100-test.test')
-
 paragrafos = todos_paragrafos(documentos)
 
-def completo(paragrafos, n, saida):
-    random.shuffle(paragrafos)
-
-    treino = paragrafos[0:n]
-
-    print_file(treino, saida)
-
-# pt-br = 569
-# pt-pt = 3304
-
-if opcao == 'holdout':
-    treino_teste(paragrafos, 2000)
-elif opcao == 'ptbr':
-    completo(paragrafos, 569, 'tmp/spacy/ptbr.train')
-elif opcao == 'ptpt':
-    completo(paragrafos, 569, 'tmp/spacy/ptpt.train')
-elif opcao == 'completo':
-    completo(paragrafos, 569, 'tmp/spacy/harem.train')
-elif opcao == 'completo-test':
-    completo(paragrafos, 569, 'tmp/spacy/harem-test.test')
-
+cli(paragrafos, opcao, variante, 'tmp/spacy/', print_ps, stdout, porcentagem)
--- opennlp.py
+++ opennlp.py
@@ -11,6 +11,33 @@ porcentagem = sys.argv[4]
 
 f = open(filename, 'r')
 
+def palavras_por_paragrafo(paragrafos):
+    c = []
+    for p in paragrafos:
+        ps = 0
+        for t in p.lista:
+            if t.t == 'em':
+                ps += 1
+            else:
+                ps += 1
+        c.append(ps)
+    return c
+
+def entidades_por_paragrafo(paragrafos):
+    c = []
+    for p in paragrafos:
+        ps = 0
+        for t in p.lista:
+            if t.t == 'em':
+                ps += 1
+        c.append(ps)
+    return c
+
+def media(paragrafos, func):
+    x = list(filter(lambda x : x != 0, func(paragrafos)))
+
+    return sum(x) / len(x)
+
 def print_ps(paragrafos):
     for p in paragrafos:
         for t in p.lista:
@@ -26,4 +53,6 @@ def print_ps(paragrafos):
 documentos = get_docs(f)
 paragrafos = todos_paragrafos(documentos)
 
-cli(paragrafos, opcao, variante, 'tmp/opennlp/', print_ps, stdout, porcentagem)
+# print(media(paragrafos, entidades_por_paragrafo))
+
+# cli(paragrafos, opcao, variante, 'tmp/opennlp/', print_ps, stdout, porcentagem)
