6debe213b9f8286791365d7d3dbb743e01fb9f53
Breno Pinheiro <davepinh@gmail.com>
2020-06-27 21:08:53 -0300

    Melhoria na geração dos arquivos de treino e teste do opennlp

--- harem.py
+++ harem.py
@@ -1,4 +1,7 @@
 import bs4
+from bs4 import BeautifulSoup
+import random
+import sys
 
 class Doc:
     def __init__(self, tag):
@@ -29,3 +32,84 @@ class Em:
             self.id = ''
             self.tipo = ''
             self.t = 'plain_text'
+
+def get_docs(f):
+    soup = BeautifulSoup(f, 'lxml')
+    docs = soup.find_all('doc')
+
+    documentos = []
+
+    for doc in docs:
+        documentos.append(Doc(doc))
+
+    return documentos
+
+def todos_paragrafos(docs):
+    par = []
+    for doc in docs:
+        for p in doc.paragrafos:
+            par.append(p)
+
+    return par
+
+def print_file(paragrafos, arquivo, print_ps, stdout):
+    with open(arquivo, 'w') as f:
+        sys.stdout = f
+        print_ps(paragrafos)
+        sys.stdout = stdout
+
+def treino_teste(paragrafos, n, out_treino, out_teste, print_f, stdout):
+    random.shuffle(paragrafos)
+
+    treino = paragrafos[0:n]
+    teste = paragrafos[n+1:len(paragrafos)]
+
+    print_file(treino, out_treino, print_f, stdout)
+    print_file(teste, out_teste, print_f, stdout)
+
+def completo_mix(paragrafos, n, out_treino, out_teste, print_f, stdout):
+    random.shuffle(paragrafos)
+
+    treino = paragrafos[0:n]
+    teste = paragrafos[n+1:n+n]
+
+    print_file(treino, out_treino, print_f, stdout)
+    print_file(teste, out_teste, print_f, stdout)
+
+def completo(paragrafos, n, saida, print_f, stdout):
+    random.shuffle(paragrafos)
+
+    treino = paragrafos[0:n]
+
+    print_file(treino, saida, print_f, stdout)
+
+def cli(paragrafos, opcao, variante, caminho, print_f, stdout, porcentagem=0.7):
+    if opcao == 'holdout':
+        p = float(porcentagem)
+        if variante == 'ptbr':
+            treino_teste(paragrafos, round(len(paragrafos)*p), caminho+'ptbr-holdout.train', caminho+'ptbr-holdout.test', print_f, stdout)
+        elif variante == 'ptpt':
+            treino_teste(paragrafos, round(len(paragrafos)*p), caminho+'ptpt-holdout.train', caminho+'ptpt-holdout.test', print_f, stdout)
+        else:
+            treino_teste(paragrafos, round(len(paragrafos)*p), caminho+'harem.train', caminho+'harem.test', print_f, stdout)
+    elif opcao == 'completo':
+        if variante == 'ptbr':
+            completo(paragrafos, 569, caminho+'ptbr.train', print_f, stdout)
+        elif variante == 'ptpt':
+            completo(paragrafos, 569, caminho+'ptpt.train', print_f, stdout)
+        elif variante == 'mix':
+            completo_mix(paragrafos, 569, caminho+'harem.train', caminho+'harem.test', print_f, stdout)
+    else:
+        ajuda()
+
+def ajuda():
+        print('Instruções de uso: opennlp.py [arquivo-harem] [opcao] [variante] [porcentagem]')
+        print('opcao:')
+        print('\tholdout')
+        print('\tcompleto')
+        print('variante:')
+        print('\tptbr')
+        print('\tptpt')
+        print('\tmix')
+        print('porcentagem:')
+        print('\t0.0 - 1.0')
--- opennlp.py
+++ opennlp.py
@@ -1,41 +1,15 @@
 import bs4
-import sys
-import random
-from bs4 import BeautifulSoup
 from harem import *
+import sys
 
 stdout = sys.stdout
 
-# filename = 'harem.xml'
-opcao = sys.argv[1]
-filename = sys.argv[2]
+filename = sys.argv[1]
+opcao = sys.argv[2]
+variante = sys.argv[3]
+porcentagem = sys.argv[4]
 
 f = open(filename, 'r')
-
-soup = BeautifulSoup(f, 'lxml')
-
-docs = soup.find_all('doc')
-
-documentos = []
-
-for doc in docs:
-    documentos.append(Doc(doc))
-
-# for doc in documentos:
-#     for p in doc.paragrafos:
-#         for t in p.lista:
-#             if t.t == 'em':
-#                 print('<START:{}> {} <END>'.format(t.categoria, t.texto), end='')
-#             else:
-#                 print(t.texto, end='')
-
-def todos_paragrafos(docs):
-    par = []
-    for doc in docs:
-        for p in doc.paragrafos:
-            par.append(p)
-
-    return par
 
 def print_ps(paragrafos):
     for p in paragrafos:
@@ -49,40 +23,7 @@ def print_ps(paragrafos):
             else:
                 print(t.texto, end='')
 
-def print_file(paragrafos, arquivo):
-    with open(arquivo, 'w') as f:
-        sys.stdout = f
-        print_ps(paragrafos)
-        sys.stdout = stdout
-
-def treino_teste(paragrafos, n):
-    random.shuffle(paragrafos)
-
-    treino = paragrafos[0:n]
-    teste = paragrafos[n+1:len(paragrafos)]
-
-    print_file(treino, 'tmp/opennlp/harem-100.train')
-    print_file(teste, 'tmp/opennlp/harem-100.test')
-
-def completo(paragrafos, n, saida):
-    random.shuffle(paragrafos)
-
-    treino = paragrafos[0:n]
-
-    print_file(treino, saida)
-
+documentos = get_docs(f)
 paragrafos = todos_paragrafos(documentos)
 
-# pt-br = 569
-# pt-pt = 3304
-
-if opcao == 'holdout':
-    treino_teste(paragrafos, 2000)
-elif opcao == 'ptbr':
-    completo(paragrafos, 569, 'tmp/opennlp/ptbr.train')
-elif opcao == 'ptpt':
-    completo(paragrafos, 569, 'tmp/opennlp/ptpt.train')
-elif opcao == 'completo':
-    completo(paragrafos, 569, 'tmp/opennlp/harem.train')
-elif opcao == 'completo-test':
-    completo(paragrafos, 569, 'tmp/opennlp/harem.test')
+cli(paragrafos, opcao, variante, 'tmp/opennlp/', print_ps, stdout, porcentagem)
