3319827e031e561bf0c3ef865962cdc48e598b46
Breno Pinheiro <davepinh@gmail.com>
2020-07-01 19:13:35 -0300

    Testes com CoreNLP

--- corenlp.py
+++ corenlp.py
@@ -29,8 +29,64 @@ def print_ps(paragrafos):
                 for tok in tokens:
                     print('{}\t{}'.format(tok, 'O'))
 
+def print_ps_sem_acento(paragrafos):
+    for p in paragrafos:
+        for t in p.lista:
+            if t.t == 'em':
+                cat = t.categoria
+                if t.categoria != None and '|' in t.categoria:
+                    cat = t.categoria.split('|')[0]
+
+                tokens = t.texto.split()
+
+                for tok in tokens:
+                    print('{}\t{}'.format(remover_acentuacao(tok), cat))
+            else:
+                tokens = t.texto.split()
+
+                for tok in tokens:
+                    print('{}\t{}'.format(remover_acentuacao(tok), 'O'))
+
+def print_docs(documentos):
+    for doc in documentos:
+        for p in doc.paragrafos:
+            for t in p.lista:
+                if t.t == 'em':
+                    cat = t.categoria
+                    if t.categoria != None and '|' in t.categoria:
+                        cat = t.categoria.split('|')[0]
+
+                    tokens = t.texto.split()
+
+                    for tok in tokens:
+                        print('{}\t{}'.format(tok, cat))
+                else:
+                    tokens = t.texto.split()
+
+                    for tok in tokens:
+                        print('{}\t{}'.format(tok, 'O'))
+
+def print_docs_sem_acento(documentos):
+    for doc in documentos:
+        for p in doc.paragrafos:
+            for t in p.lista:
+                if t.t == 'em':
+                    cat = t.categoria
+                    if t.categoria != None and '|' in t.categoria:
+                        cat = t.categoria.split('|')[0]
+
+                    tokens = t.texto.split()
+
+                    for tok in tokens:
+                        print('{}\t{}'.format(remover_acentuacao(tok), cat))
+                else:
+                    tokens = t.texto.split()
+
+                    for tok in tokens:
+                        print('{}\t{}'.format(remover_acentuacao(tok), 'O'))
 
 documentos = get_docs(f)
 paragrafos = todos_paragrafos(documentos)
 
+# cli(documentos, opcao, variante, 'tmp/corenlp/', print_docs, stdout, porcentagem)
 cli(paragrafos, opcao, variante, 'tmp/corenlp/', print_ps, stdout, porcentagem)
